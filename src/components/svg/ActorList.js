782bcb8f61f92084985a35e1c14c78455aa35c9a
45deg <45deg@users.noreply.github.com>
2016-09-14 21:08:33 +0900

    Separated ActorList from Diagram

---
+++ src/components/svg/ActorList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Actor from './Actor';
+
+const ActorList = ({ actors, timeSpan, margin, width, messageNum }) => {
+  return <g id="actor-container">{
+    actors.map((actor, i) =>
+      <Actor x={width / actors.length * actor.pid}
+        textY={18}
+        lineStartY={actor._up * timeSpan + margin}
+        lineEndY={actor._down ? actor._down * timeSpan + margin : (messageNum + 1) * timeSpan + margin}
+        text={actor.constructor.name + "\n#" + actor.pid}
+        key={i} />
+    ) }</g>;
+}
+
+function mapStateToProps(state) {
+  return {
+    actors: state.vm.actors,
+    messageNum: state.vm.messageLog.length,
+    width: state.panels['root-panel']
+  };
+}
+
+export default connect(mapStateToProps)(ActorList);

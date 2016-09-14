36338f7a97261fd721fea21a5b724140e0444c1d
45deg <45deg@users.noreply.github.com>
2016-09-15 00:09:16 +0900

    Implemented flush (dirty)

--- src/components/StepField.js
+++ src/components/StepField.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { stepActor, backActor } from '../actions/vm';
+import store from '../store';
 
 // maybe this is called container
 
-const StepField = ({ step, back, messages, actors, clock }) => {
+const StepField = ({ step, back, sendAll, messages, actors, clock }) => {
     return (<form id="controller">
         <p id="stepfield">
         {
@@ -17,7 +18,9 @@ const StepField = ({ step, back, messages, actors, clock }) => {
         }
         </p>
         <p>
-            { clock > 0 ? <input type="button" className="btn-control" value="back" onClick={back} /> : ''}
+            { messages.length > 0 ? <input type="button" className="btn-control" value="flush" onClick={
+                () => sendAll(messages)} /> : null}
+            { clock > 0 ? <input type="button" className="btn-control" value="back" onClick={back} /> : null}
         </p>
     </form>);
 }
@@ -36,6 +39,15 @@ function mapDispatchToProps(dispatch) {
       var target = actors[msg.to];
       dispatch(stepActor(index));
       target[target._state](...msg.data);
+    },
+    sendAll: (messages) => {
+      messages.forEach((msg) => {
+        var target = store.getState().vm.actors[msg.to];
+        var index = store.getState().vm.messageQueue.findIndex(m => m.uid === msg.uid);
+        console.log(index);
+        dispatch(stepActor(index));
+        target[target._state](...msg.data);
+      });
     },
     back: () => dispatch(backActor())
   };

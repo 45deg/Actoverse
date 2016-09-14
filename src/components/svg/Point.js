347aaadbc75675dd129d978ee90579bb78086766
45deg <45deg@users.noreply.github.com>
2016-09-15 01:54:42 +0900

    Simplify Actor Point

--- src/components/svg/Point.js
+++ src/components/svg/Point.js
@@ -8,31 +8,13 @@ const Point = ({ cx, cy, moveBack, showToolTip, hideToolTip }) => {
 import { backActor } from '../../actions/vm';
 import { showToolTip, hideToolTip } from '../../actions/diagram';
 
-function mapStateToProps(state) {
-    return {
-        actors: state.vm.actors,
-        history: state.vm.history,
-    };
-}
 
 function mapDispatchToProps(dispatch, ownProps) {
     return {
       moveBack: () => dispatch(backActor(ownProps.backCount)),
-      showToolTipDispatch: (x, y, actor) => dispatch(showToolTip(x, y, actor)),
+      showToolTip: (e) => dispatch(showToolTip(e.pageX, e.pageY, ownProps.actor)),
       hideToolTip: () => dispatch(hideToolTip()),
     }
 }
 
-function mergeProps(stateProps, dispatchProps, ownProps) {
-  return Object.assign({}, ownProps, dispatchProps, {
-    showToolTip: (e) => {
-      var {clock, actors, history} = stateProps;
-      var {backCount, actorPid} = ownProps;
-      var actor = backCount === 0 ? actors[actorPid]
-                                  : history[history.length-backCount].actors[actorPid];
-      dispatchProps.showToolTipDispatch(e.pageX, e.pageY, actor)
-    }
-  });
-}
-
-export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(Point);
+export default connect(null, mapDispatchToProps)(Point);
--- src/components/svg/PointList.js
+++ src/components/svg/PointList.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Point from './Point';
 
-const PointList = ({ timeSpan, margin, messageLog, width, actorNum }) => {
+const PointList = ({ timeSpan, margin, messageLog, width, actors }) => {
   return <g>{
-    messageLog.map((msg, index) =>
-      <Point cx={ width / actorNum * msg.to }
+    messageLog.map((msg, index) => {
+      var actor;
+      if( messageLog.length - 1 === index )
+        actor = actors[msg.to];
+      else
+        actor = history[index + 1].actors[msg.to];
+
+      return <Point cx={ width / actor.length * msg.to }
         cy={ (index + 1) * timeSpan + margin }
-        backCount={ messageLog.length - index - 1 }
-        actorPid={msg.to} key={index} />
-    ) }</g>;
+        backCount={messageLog.length - index - 1}
+        actor={actor} key={msg.uid} />;
+    })
+  }</g>;
 }
 function mapStateToProps(state) {
   return {
-    actorNum: state.vm.actors.length,
+    actors: state.vm.actors,
     messageLog: state.vm.messageLog,
     width: state.panels['root-panel'],
     timeSpan: state.diagram.timeSpan,

ee1d39ed1d811a71746732e1a8bb9705572ce34a
Kazuhiro Shibanai <shibanai@dhcp110.psg.private>
2016-09-15 17:50:00 +0900

    Fixed slider

--- src/js/components/ConfigPanel.js
+++ src/js/components/ConfigPanel.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { updateTimeSpan, toggleMessage } from '../actions/diagram';
 
+import { FormGroup, Checkbox, Col } from 'react-bootstrap';
+
+import 'css/debug';
+
 const ConfigPanel = ({ size, width, messageFlag, updateTimeSpan, toggleMessage }) => {
   return (<div id="config-panel">
-    <p>
-      <input type="checkbox" id="toggle-message" checked={messageFlag}
-        onChange={e => toggleMessage(e.target.checked)}  />
-        <label htmlFor="toggle-message">Show all messages</label>
-    </p>
-    <p>
-      Interval size: <input type="range" min="1" max={width/3} value={size}
+    <form>
+    <FormGroup>
+      <Checkbox checked={messageFlag}　onChange={e => toggleMessage(e.target.checked)}>Show all messages</Checkbox>
+    </FormGroup>
+    <FormGroup controlId="formHorizontalPassword">
+      <Col sm={3}>
+        IntervalSize
+      </Col>
+      <Col sm={12-3}>
+        <input type="range" min="1" max={width/3} value={size} className="slider"
                        onChange={e => updateTimeSpan(e.target.value)} />
-        <span>{size}</span>
-    </p>
+                        <span>{size}</span>
+      </Col>
+    </FormGroup>
+    </form>
   </div>);
 }
 

2d924b97fb312b8caf6260f538f5f3cae53d83ed
45deg <45deg@users.noreply.github.com>
2016-10-16 16:49:00 +0900

    Modified toolbar for new method

--- src/js/components/ToolBar.js
+++ src/js/components/ToolBar.js
@@ -1,15 +1,53 @@
 import React from 'react';
 import { connect } from 'react-redux'
 
-import { Button, Glyphicon } from 'react-bootstrap';
-import {initActor} from '../actions/shadow';
+import { Button, Glyphicon, Form, FormControl, FormGroup, ControlLabel } from 'react-bootstrap';
+import { connectNetwork, disconnectNetwork } from '../actions/network';
 
 import 'css/toolbar';
 
-const ToolBar = ({ submitCode }) => {
+class ToolBar extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { target: '' };
+  }
+  onSubmit(){
+    if(!this.props.connected) {
+      this.props.connect(this.state.target);
+    } else {
+      this.props.disconnect();
+    }
+  }
+  render(){
     return (<div className="toolbar-panel">
-              <Button bsStyle="primary" bsSize="large"><Glyphicon glyph="play" /> Run</Button>
-            </div>);
+      <Form inline>
+        <FormGroup>
+          <ControlLabel>Target</ControlLabel>
+          {' '}
+          <FormControl type="text" placeholder="Ex. localhost:????"
+            value={ this.state.target }
+            onChange={e => this.setState({ target: e.target.value })}/>
+        </FormGroup>
+        <Button bsStyle="primary" onClick={this.onSubmit.bind(this)}>
+          { !this.props.connected ? 'Connect' : 'Disconnect' }
+       </Button>
+      </Form>
+    </div>);
+  }
 };
 
-export default ToolBar;
+function mapStateToProps(state) {
+  return {
+    connected: state.network.connected,
+    target: state.network.target,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    connect: (target) => dispatch(connectNetwork(target)),
+    disconnect: () => dispatch(disconnectNetwork()),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ToolBar);

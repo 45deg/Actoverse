9a8ade51a572c88e4524f1297cb5d8550db42cfb
45deg <45deg@users.noreply.github.com>
2016-10-16 10:51:46 +0900

    Fixed actions bug

--- src/js/actions/network.js
+++ src/js/actions/network.js
@@ -2,6 +2,6 @@ export function connectNetwork(target){
     return { type: 'CONNECT_NETWORK', target };
 }
 
-export function disconnectNetwork(panelName, size){
-    return { type: 'DISCONNECT_NETWORK', target };
+export function disconnectNetwork(){
+    return { type: 'DISCONNECT_NETWORK' };
 }

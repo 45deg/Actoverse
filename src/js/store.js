753b01bccdd0ad35d8ce53e376165513a4b72ad6
45deg <45deg@users.noreply.github.com>
2016-10-16 17:23:31 +0900

    Added filter of logging

--- src/js/store.js
+++ src/js/store.js
@@ -2,7 +2,9 @@ import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import createLogger from 'redux-logger';
 
-const loggerMiddleware = createLogger();
+const loggerMiddleware = createLogger({
+  predicate: (getState, action) => action.type !== 'CHANGE_SIZE'
+});
 
 const store = createStore(reducers, applyMiddleware(loggerMiddleware));
 export default store;

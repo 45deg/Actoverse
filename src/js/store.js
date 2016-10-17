acb88a9532330bd1c5826dc7927ebc611a07b326
45deg <45deg@users.noreply.github.com>
2016-10-17 14:48:13 +0900

    Introduced redux-thunk

--- src/js/store.js
+++ src/js/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import createLogger from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 
 const ignoreEvents = [
   'CHANGE_SIZE',
@@ -10,5 +11,8 @@ const loggerMiddleware = createLogger({
   predicate: (getState, action) => !ignoreEvents.includes(action.type)
 });
 
-const store = createStore(reducers, applyMiddleware(loggerMiddleware));
+const store = createStore(
+  reducers,
+  applyMiddleware(loggerMiddleware, thunkMiddleware)
+);
 export default store;

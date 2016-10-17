c2962776844ae98cd96f7a728dd9a76fc802ebf9
45deg <45deg@users.noreply.github.com>
2016-10-17 14:15:38 +0900

    Added filter of redux logger

--- src/js/store.js
+++ src/js/store.js
@@ -2,8 +2,12 @@ import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import createLogger from 'redux-logger';
 
+const ignoreEvents = [
+  'CHANGE_SIZE',
+  'MOVE_TO_FRONT'
+]
 const loggerMiddleware = createLogger({
-  predicate: (getState, action) => action.type !== 'CHANGE_SIZE'
+  predicate: (getState, action) => !ignoreEvents.includes(action.type)
 });
 
 const store = createStore(reducers, applyMiddleware(loggerMiddleware));

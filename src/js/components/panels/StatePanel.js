931ee1e02cd40f469b7146ce423b57d2d8d8214b
45deg <shibanai@dhcp110.psg.private>
2016-10-18 14:43:46 +0900

    Refined StatePanel

--- src/js/components/panels/StatePanel.js
+++ src/js/components/panels/StatePanel.js
@@ -1,41 +1,74 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Table } from 'react-bootstrap';
 
+import 'css/state-panel';
+
+const StateTable = ({state}) => {
+  return <Table striped bordered condensed hover>
+    <caption>State</caption>
+    <thead><tr>
+      <th>Key</th><th>Value</th>
+    </tr></thead>
+    <tbody>{
+      state.map((value, key) =>
+        <tr key={key}><td>{key}</td><td>{JSON.stringify(value)}</td></tr>
+      )
+    }</tbody>
+  </Table>;
+};
+
+const MailboxTable = ({mailbox}) => {
+  return <Table striped bordered condensed hover>
+    <caption>Mailbox</caption>
+    <thead><tr>
+      <th>From</th><th>To</th><th>Data</th>
+    </tr></thead>
+    <tbody>{
+      mailbox.map((mail, key) =>
+        <tr key={key}>
+          <td>{mail.get('sender')}</td>
+          <td>{mail.get('target')}</td>
+          <td>{JSON.stringify(mail.get('data'))}</td>
+        </tr>
+      )
+    }</tbody>
+   </Table>;
+};
+
+class ActorEntry extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { showDetails: false };
+  }
+
+  toggleDetails(){
+    this.setState({ showDetails: !this.state.showDetails });
+  }
+
+  render(){
+    var actor = this.props.actor;
+    return <div>
+      <h2 className={ this.state.showDetails ? 'tables-open' : 'tables-closed'}
+          onClick={this.toggleDetails.bind(this)}>
+        { actor.get('name') } #{ actor.get('pid') }
+      </h2>
+      { this.state.showDetails &&
+        <div className="tables-container">
+          <StateTable state={actor.get('state')} />
+          <MailboxTable mailbox={actor.get('mailbox')} />
+        </div>
+      }
+    </div>;
+  }
+}
+
 const StatePanel = ({actors}) => {
-  return (<div>{
-    actors.map((actor, key) => {
-      return <div key={key}>
-        <h2>{ actor.get('name') } #{ actor.get('pid') }</h2>
-        <Table striped bordered condensed hover>
-          <caption>State</caption>
-          <thead><tr>
-            <td>Key</td><td>Value</td>
-          </tr></thead>
-          <tbody>{
-            actor.get('state').map((value, key) =>
-              <tr key={key}><td>{key}</td><td>{JSON.stringify(value)}</td></tr>
-            )
-          }</tbody>
-         </Table>
-         <Table striped bordered condensed hover>
-           <caption>Mailbox</caption>
-           <thead><tr>
-             <td>From</td><td>To</td><td>Data</td>
-           </tr></thead>
-           <tbody>{
-             actor.get('mailbox').map((mail, key) =>
-               <tr key={key}>
-                 <td>{mail.get('sender')}</td>
-                 <td>{mail.get('target')}</td>
-                 <td>{JSON.stringify(mail.get('data'))}</td>
-               </tr>
-             )
-           }</tbody>
-          </Table>
-      </div>;
-    }).toArray()
-  }</div>);
+  return <section id="state-panel">{
+    actors.map((actor, key) =>
+      <ActorEntry actor={actor} key={key} />).toArray()
+  }</section>;
 }
 
 function mapStateToProps(state) {

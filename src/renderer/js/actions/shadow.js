609a0b7fc1cea3e4438d37ac4bd29ea8698006be
45deg <shibanai@dhcp110.psg.private>
2016-10-24 18:57:06 +0900

    Adopted for rollback api change

--- src/renderer/js/actions/shadow.js
+++ src/renderer/js/actions/shadow.js
@@ -3,16 +3,10 @@ import store from '../store';
 
 export function rollbackTime(time){
   return dispatch => {
-    var shadow = store.getState().shadow;
-    var actorAtTime = shadow.actorSnapshots[time - 1];
-    for(let actor of actorAtTime) {
-      socket.send({
-        type: 'replace',
-        target: actor.get('pid'),
-        data: actor.toJS()
-      });
-    }
-    dispatch({type:'ROLLBACK_TIME', time});
+    socket.send({
+      type: 'rollback',
+      time
+    });
   };
 }
 

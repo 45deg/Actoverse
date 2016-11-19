4bdb05295c310f484b19c39738dd020ca6432901
45deg <45deg@users.noreply.github.com>
2016-11-19 21:54:47 +0900

    Fixed using clock instead of messageNum

--- src/renderer/js/components/diagram/ActorList.js
+++ src/renderer/js/components/diagram/ActorList.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Actor from './Actor';
 
-const ActorList = ({ actors, timeInterval, margin, width, messageNum }) => {
+const ActorList = ({ actors, timeInterval, margin, width, clock }) => {
   return <g id="actor-container">{
     actors.valueSeq().map((actor, i) =>
       <Actor x={ width / (actors.size + 1) * (i + 1) }
         textY={18}
         lineStartY={margin /* TODO: implement uptime */}
-        lineEndY={(messageNum + 1) * timeInterval + margin}
+        lineEndY={(clock + 1) * timeInterval + margin}
         name={actor.get('name')}
         pid={actor.get('pid')}
         key={actor.get('pid')} />
@@ -18,7 +18,7 @@ const ActorList = ({ actors, timeInterval, margin, width, messageNum }) => {
 function mapStateToProps(state) {
   return {
     actors: state.shadow.actors,
-    messageNum: state.shadow.messageLog.length,
+    clock: state.shadow.clock,
     width: state.ui.panelSize['root-panel'],
     timeInterval: state.ui.timeInterval,
   };

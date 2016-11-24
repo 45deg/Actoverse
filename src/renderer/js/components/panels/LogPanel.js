a981893a9386df669f301893e384b62b5fb77a87
45deg <45deg@users.noreply.github.com>
2016-11-24 22:01:32 +0900

    Showing sender/receiver infos in detail

--- src/renderer/js/components/panels/LogPanel.js
+++ src/renderer/js/components/panels/LogPanel.js
@@ -1,34 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Table } from 'react-bootstrap';
+import { Table, Button } from 'react-bootstrap';
 
-const LogPanel = ({ messageLogs }) => {
-  console.log(messageLogs.valueSeq().flatten(true).toJS())
-  return <Table striped bordered condensed hover>
-    <caption>State</caption>
-    <thead><tr>
-      <th>LC</th><th>Type</th><th>Sender</th><th>Receiver</th><th>Data</th>
-    </tr></thead>
-    <tbody>{
-      messageLogs.valueSeq().flatten(true)
-      .sortBy(entry => entry.get('timestamp'))
-      .map((entry, idx) => {
-        let body = entry.get('body');
-        return <tr key={idx}>
-          <td>{entry.get('timestamp')}</td>
-          <td>{entry.get('type')}</td>
-          <td>{body.get('sender')}</td>
-          <td>{body.get('target')}</td>
-          <td>{JSON.stringify(body.get('data').toJS())}</td>
-        </tr>;
-      })
-    }</tbody>
-  </Table>;
+const LogPanel = ({ actors,messageLogs }) => {
+  return <div style={{margin: '5px'}}>
+  <Table striped bordered condensed hover>
+  <caption>State</caption>
+  <thead><tr>
+  <th>LC</th><th>Type</th><th>Sender</th><th>Receiver</th><th>Data</th>
+  </tr></thead>
+  <tbody>{
+    messageLogs.valueSeq().flatten(true)
+    .sortBy(entry => entry.get('timestamp'))
+    .map((entry, idx) => {
+      let body = entry.get('body');
+      return <tr key={idx}>
+      <td>{entry.get('timestamp')}</td>
+      <td>{entry.get('type')}</td>
+      <td>{actors.getIn([body.get('sender'), 'class'], 'master')}#{body.get('sender')}</td>
+      <td>{actors.getIn([body.get('target'), 'class'], 'master')}#{body.get('target')}</td>
+      <td>{JSON.stringify(body.get('data').toJS())}</td>
+      </tr>;
+    })
+  }</tbody>
+  </Table>
+  </div>;
 };
 
 function mapStateToProps(state) {
   return {
-    messageLogs: state.shadow.messageLogs
+    messageLogs: state.shadow.messageLogs,
+    actors: state.shadow.actors,
   };
 }
 
